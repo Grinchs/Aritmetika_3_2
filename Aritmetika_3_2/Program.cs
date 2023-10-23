c80f96dba3f2dc118037185ed2e4a827c620f2bc
Grinchs <marcisg2345@gmail.com>
2023-10-23 22:32:38 +0300

    Add project files.

---
+++ Aritmetika_3_2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ievadiet vārdu: ");
+        string vards = Console.ReadLine();
+
+        Console.Write("Ievadiet uzvārdu: ");
+        string uzvards = Console.ReadLine();
+
+        string sveiciens = "Sveiks " + vards + " " + uzvards + "!";
+        Console.WriteLine(sveiciens);      
+    }
+}
